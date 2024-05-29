93203ca72d9256dc405b63c77c2b14ea6dbb2f12
KatarinaB96 <katarinabartolovic2501@gmail.com>
2024-05-29 11:29:27 +0200

    Custom ClickOnceButton that only allows a single click

--- app/src/main/java/com/dream/presentation/PageCurlScreen.kt
+++ app/src/main/java/com/dream/presentation/PageCurlScreen.kt
@@ -1,18 +1,32 @@
 package com.dream.presentation
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ButtonElevation
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
@@ -55,18 +69,18 @@ fun PageCurlScreen(navController: NavController) {
         ) { index ->
             StoryPage(title = pages[index], story = stories[index], image = images[index])
         }
-        Button(
-            onClick = {
-                 navController.popBackStack()
-            },
-            modifier = Modifier
-                .align(Alignment.TopStart)
-                .padding(16.dp)
-        ) {
-            Text("<")
+
+        ClickOnceButton(onClick = {
+            navController.popBackStack()
+        }) {
+
+            Image(
+                painter = painterResource(id = R.drawable.ic_back_arrow),
+                contentDescription = "Back Arrow",
+                modifier = Modifier.size(34.dp)
+            )
         }
     }
-
 }
 
 @Composable
@@ -102,4 +116,41 @@ fun StoryPage(title: String, story: String, image: Painter) {
             )
         }
     }
+}
+
+@Composable
+fun ClickOnceButton(
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    conditional: () -> Boolean = { true },
+    enabled: Boolean = true,
+    interactionSource: MutableInteractionSource = remember { MutableInteractionSource() },
+    elevation: ButtonElevation? = ButtonDefaults.buttonElevation(),
+    shape: Shape = MaterialTheme.shapes.small,
+    border: BorderStroke? = null,
+    buttonColors: ButtonColors = ButtonDefaults.buttonColors(
+        Color.Transparent
+    ),
+    contentPadding: PaddingValues = ButtonDefaults.ContentPadding,
+    content: @Composable RowScope.() -> Unit
+) {
+    var onClickHasExecuted by remember { mutableStateOf(false) }
+
+    Button(
+        onClick = {
+            if (conditional() && !onClickHasExecuted) {
+                onClickHasExecuted = true
+                onClick()
+            }
+        },
+        modifier = modifier,
+        enabled = enabled,
+        interactionSource = interactionSource,
+        elevation = elevation,
+        shape = shape,
+        border = border,
+        colors = buttonColors,
+        contentPadding = contentPadding,
+        content = content
+    )
 }
